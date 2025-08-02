6df415455eedb8cd8504dd0dfd95d0432409bd3f
Cryptid <cryptidbleh@icloud.com>
2025-08-02 18:13:25 +0200

    Show site titles and favicons on tabs

--- gui.cpp
+++ gui.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <windowsx.h>
 #include <commctrl.h>
+#include <urlmon.h>
 #include <vector>
 #include <WebKit/WebKit2_C.h>
 #include <string>
@@ -9,11 +10,12 @@
 #include "settings.h"
 
 #pragma comment(lib, "Comctl32.lib")
+#pragma comment(lib, "Urlmon.lib")
 
 static std::string MakeUserAgent()
 {
     std::wstring wver = BuildInfo::kVersionRaw;
-    return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15 Cryptidium/" +
+    return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Cryptidium/" +
            std::string(wver.begin(), wver.end());
 }
 
@@ -27,6 +29,8 @@ HFONT GetUIFont()
 
 struct Tab {
     WKViewRef view;
+    HWND closeBtn;
+    int imageIndex;
 };
 
 static std::vector<Tab> gTabs;
@@ -39,6 +43,7 @@ static HWND gForwardBtn;
 static HWND gRefreshBtn;
 static HWND gNewTabBtn;
 static HWND gSettingsBtn;
+static HIMAGELIST gTabImages;
 
 static const int TAB_HEIGHT = 24;
 static const int NAV_HEIGHT = 28;
@@ -68,6 +73,61 @@ static void UpdateUrlBarFromPage(WKPageRef page)
     SetWindowTextW(gUrlBar, wtext.c_str());
 }
 
+static std::string GetHostFromURL(const std::string& url)
+{
+    size_t scheme_end = url.find("://");
+    if (scheme_end == std::string::npos)
+        return {};
+    size_t host_start = scheme_end + 3;
+    size_t host_end = url.find('/', host_start);
+    return url.substr(host_start, host_end - host_start);
+}
+
+static void UpdateTabFromPage(WKPageRef page)
+{
+    for (size_t i = 0; i < gTabs.size(); ++i) {
+        if (WKViewGetPage(gTabs[i].view) != page)
+            continue;
+
+        WKStringRef titleRef = WKPageCopyTitle(page);
+        size_t tmax = WKStringGetMaximumUTF8CStringSize(titleRef);
+        std::string tbuf(tmax, '\0');
+        WKStringGetUTF8CString(titleRef, tbuf.data(), tmax);
+        std::string title = tbuf.c_str();
+        std::wstring wtitle(title.begin(), title.end());
+        WKRelease(titleRef);
+        TCITEMW tie{};
+        tie.mask = TCIF_TEXT;
+        tie.pszText = wtitle.data();
+        TabCtrl_SetItem(gTabCtrl, i, &tie);
+
+        WKURLRef urlRef = WKPageCopyActiveURL(page);
+        if (urlRef) {
+            WKStringRef str = WKURLCopyString(urlRef);
+            size_t umax = WKStringGetMaximumUTF8CStringSize(str);
+            std::string ubuf(umax, '\0');
+            WKStringGetUTF8CString(str, ubuf.data(), umax);
+            WKRelease(str);
+            WKRelease(urlRef);
+            std::string url = ubuf.c_str();
+            std::string host = GetHostFromURL(url);
+            if (!host.empty()) {
+                std::string iconUrl = "https://" + host + "/favicon.ico";
+                std::wstring wicon(iconUrl.begin(), iconUrl.end());
+                wchar_t cachePath[MAX_PATH];
+                if (SUCCEEDED(URLDownloadToCacheFileW(nullptr, wicon.c_str(), cachePath, MAX_PATH, 0, nullptr))) {
+                    HICON hico = (HICON)LoadImageW(nullptr, cachePath, IMAGE_ICON, 16, 16, LR_LOADFROMFILE);
+                    if (hico) {
+                        ImageList_ReplaceIcon(gTabImages, gTabs[i].imageIndex, hico);
+                        DestroyIcon(hico);
+                    }
+                }
+            }
+        }
+        break;
+    }
+}
+
 
 static void ShowCurrentTab()
 {
@@ -87,12 +147,17 @@ static void ResizeChildren(HWND hWnd)
     MoveWindow(gBackBtn, 0, y, 30, NAV_HEIGHT, TRUE);
     MoveWindow(gForwardBtn, 30, y, 30, NAV_HEIGHT, TRUE);
     MoveWindow(gRefreshBtn, 60, y, 30, NAV_HEIGHT, TRUE);
-    MoveWindow(gSettingsBtn, 90, y, 30, NAV_HEIGHT, TRUE);
-    MoveWindow(gUrlBar, 120, y, rc.right - 120, NAV_HEIGHT, TRUE);
+    MoveWindow(gUrlBar, 90, y, rc.right - 120, NAV_HEIGHT, TRUE);
+    MoveWindow(gSettingsBtn, rc.right - 30, y, 30, NAV_HEIGHT, TRUE);
     int top = TAB_HEIGHT + NAV_HEIGHT;
     for (auto& t : gTabs) {
         HWND child = WKViewGetWindow(t.view);
         MoveWindow(child, 0, top, rc.right, rc.bottom - top, TRUE);
+    }
+    for (size_t i = 0; i < gTabs.size(); ++i) {
+        RECT tr;
+        TabCtrl_GetItemRect(gTabCtrl, i, &tr);
+        MoveWindow(gTabs[i].closeBtn, tr.right - 16, 4, 16, 16, TRUE);
     }
 }
 
@@ -122,18 +187,24 @@ static void AddTab(HWND hWnd, const char* url)
     navClient.base.version = 0;
     navClient.didFinishNavigation = [](WKPageRef p, WKNavigationRef, WKTypeRef, const void*) {
         UpdateUrlBarFromPage(p);
+        UpdateTabFromPage(p);
     };
     WKPageSetPageNavigationClient(page, &navClient.base);
     HWND child = WKViewGetWindow(view);
     ShowWindow(child, SW_HIDE);
     WKViewSetIsInWindow(view, true);
-    gTabs.push_back({ view });
+    HICON ph = (HICON)LoadImageW(nullptr, L"assets\\app.ico", IMAGE_ICON, 16, 16, LR_LOADFROMFILE);
+    int img = ImageList_AddIcon(gTabImages, ph);
+    DestroyIcon(ph);
+    HWND closeBtn = CreateWindowW(L"BUTTON", L"x", WS_CHILD | WS_VISIBLE,
+                                  0, 0, 16, 16, gTabCtrl, nullptr, nullptr, nullptr);
+    SendMessageW(closeBtn, WM_SETFONT, (WPARAM)gUIFont, TRUE);
+    gTabs.push_back({ view, closeBtn, img });
     int index = static_cast<int>(gTabs.size()) - 1;
     TCITEMW tie{};
-    tie.mask = TCIF_TEXT;
-    wchar_t text[32];
-    wsprintfW(text, L"Tab %d", index + 1);
-    tie.pszText = text;
+    tie.mask = TCIF_TEXT | TCIF_IMAGE;
+    tie.iImage = img;
+    tie.pszText = const_cast<wchar_t*>(L"Loading");
     TabCtrl_InsertItem(gTabCtrl, index, &tie);
     gCurrentTab = index;
     ShowCurrentTab();
@@ -141,6 +212,29 @@ static void AddTab(HWND hWnd, const char* url)
         NavigateCurrent(url);
     }
     TabCtrl_SetCurSel(gTabCtrl, gCurrentTab);
+    ResizeChildren(hWnd);
+}
+
+static void CloseTab(HWND hWnd, int index)
+{
+    if (index < 0 || index >= static_cast<int>(gTabs.size()))
+        return;
+    DestroyWindow(gTabs[index].closeBtn);
+    WKViewRef view = gTabs[index].view;
+    HWND child = WKViewGetWindow(view);
+    DestroyWindow(child);
+    WKRelease(view);
+    gTabs.erase(gTabs.begin() + index);
+    TabCtrl_DeleteItem(gTabCtrl, index);
+    if (gCurrentTab >= index)
+        gCurrentTab--;
+    if (gCurrentTab >= 0) {
+        ShowCurrentTab();
+        TabCtrl_SetCurSel(gTabCtrl, gCurrentTab);
+        UpdateUrlBarFromPage(WKViewGetPage(gTabs[gCurrentTab].view));
+    } else {
+        SetWindowTextW(gUrlBar, L"");
+    }
     ResizeChildren(hWnd);
 }
 
@@ -185,6 +279,8 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPara
         gTabCtrl = CreateWindowExW(0, WC_TABCONTROLW, L"", WS_CHILD | WS_CLIPSIBLINGS | WS_VISIBLE,
                                    0, 0, 0, 0, hWnd, (HMENU)1000, nullptr, nullptr);
         SendMessageW(gTabCtrl, WM_SETFONT, (WPARAM)gUIFont, TRUE);
+        gTabImages = ImageList_Create(16, 16, ILC_COLOR32 | ILC_MASK, 1, 1);
+        TabCtrl_SetImageList(gTabCtrl, gTabImages);
         gNewTabBtn = CreateWindowW(L"BUTTON", L"+", WS_CHILD | WS_VISIBLE,
                                    0, 0, 0, 0, hWnd, (HMENU)1001, nullptr, nullptr);
         SendMessageW(gNewTabBtn, WM_SETFONT, (WPARAM)gUIFont, TRUE);
@@ -211,6 +307,14 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPara
         ResizeChildren(hWnd);
         return 0;
     case WM_COMMAND: {
+        if ((HWND)lParam) {
+            for (size_t i = 0; i < gTabs.size(); ++i) {
+                if ((HWND)lParam == gTabs[i].closeBtn) {
+                    CloseTab(hWnd, static_cast<int>(i));
+                    return 0;
+                }
+            }
+        }
         switch (LOWORD(wParam)) {
         case 1001:
             AddTab(hWnd, "https://google.com");
@@ -223,16 +327,16 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPara
             if (gCurrentTab >= 0)
                 WKPageGoForward(WKViewGetPage(gTabs[gCurrentTab].view));
             break;
-          case 1004:
-              if (gCurrentTab >= 0)
-                  WKPageReload(WKViewGetPage(gTabs[gCurrentTab].view));
-              break;
-          case 1006:
-              ShowSettings(hWnd);
-              break;
-          }
-          return 0;
-      }
+        case 1004:
+            if (gCurrentTab >= 0)
+                WKPageReload(WKViewGetPage(gTabs[gCurrentTab].view));
+            break;
+        case 1006:
+            ShowSettings(hWnd);
+            break;
+        }
+        return 0;
+    }
     case WM_NOTIFY: {
         LPNMHDR nm = (LPNMHDR)lParam;
         if (nm->hwndFrom == gTabCtrl && nm->code == TCN_SELCHANGE) {
@@ -244,6 +348,7 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPara
         return 0;
     }
     case WM_DESTROY:
+        ImageList_Destroy(gTabImages);
         DeleteObject(gUIFont);
         PostQuitMessage(0);
         return 0;
